395a29eed8fcaded08eaf8e4adf1695ce61de7d5
shn <topdog.shn.01@gmail.com>
2022-01-07 13:00:53 +0900

    🔧 ESLintとPrettierの設定更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "plugin:react/recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "semi": "error",
-        "quotes": "error",
-        "react/prop-types": "off"
-    }
+	env: {
+		browser: true,
+		es2021: true,
+		node: "true",
+	},
+	plugins: [],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+		"prettier",
+	],
+	parserOptions: {
+		sourceType: "module",
+	},
+	rules: {
+		quotes: "error",
+		"no-undef": "error",
+		"react/prop-types": "off",
+		"react/jsx-props-no-spreading": "off",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
